efbebb70f4ac594dae2992c099cd1a7ce3125509
Carlos <miklo.concepcion@gmail.com>
2020-01-13 17:31:08 -0600

    committing before seeding data. all seeds filled out. going back over data flow.

--- data/seeds/005-projects_resources.js
+++ data/seeds/005-projects_resources.js
@@ -1,5 +1,7 @@
-// exports.seed = async (knex) => {
-//   await knex('projects_resources').insert([
-
-//       ]);
-//     });
+exports.seed = async knex => {
+  await knex("projects_resources").insert([
+    { project_Id: 1, resource_Id: 3 },
+    { project_Id: 2, resource_Id: 2 },
+    { project_Id: 3, resource_Id: 1 }
+  ]);
+};
