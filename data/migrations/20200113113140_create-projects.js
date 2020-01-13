2594c06c212e37b495ba274387b3a4dd484477cd
Carlos <miklo.concepcion@gmail.com>
2020-01-13 12:06:36 -0600

    updating to es6 and async promises for migrations/seeds files

--- data/migrations/20200113113140_create-projects.js
+++ data/migrations/20200113113140_create-projects.js
@@ -1,57 +1,56 @@
-exports.up = function(knex) {
-  return knex.schema
-    .createTable("projects", table => {
-      table.increments("id");
-      table
-        .text("name")
-        .unique()
-        .notNullable();
-      table.text("description").notNullable();
-      table.boolean("completed").defaultTo("false");
-    })
+exports.up = async knex => {
+  await knex.schema.createTable("projects", table => {
+    table.increments("id");
+    table
+      .text("name")
+      .unique()
+      .notNullable();
+    table.text("description").notNullable();
+    table.boolean("completed").defaultTo("false");
+  });
 
-    .createTable("resources", table => {
-      table.increments("id");
-      //   foreign key
-      table
-        .integer("project_id")
-        .notNullable()
-        .references("id")
-        .inTable("projects")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE");
-      table.text("description").notNullable();
-      table.text("notes").notNullable();
-      table.boolean("completed").defaultTo("false");
-    })
+  await knex.schema.createTable("resources", table => {
+    table.increments("id");
+    //   foreign key
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.text("description").notNullable();
+    table.text("notes").notNullable();
+    table.boolean("completed").defaultTo("false");
+  });
 
-    .createTable("tasks", table => {
-      table.increments("id");
-      table.string("task_description");
-      table.string("notes");
-      table
-        .boolean("completed")
-        .defaultTo(false)
-        .notNullable();
-    })
+  await knex.schema.createTable("tasks", table => {
+    table.increments("id");
+    table.string("task_description");
+    table.string("notes");
+    table
+      .boolean("completed")
+      .defaultTo(false)
+      .notNullable();
+  });
 
-    .createTable("projects_tasks", table => {
-      table
-        .integer("project_id")
-        .notNullable()
-        .references("id")
-        .inTable("projects");
-      table
-        .integer("task_id")
-        .notNullable()
-        .references("id")
-        .inTable("task");
-      table.primary(["project_id", "task_id"]);
-    });
+  await knex.schema.createTable("projects_tasks", table => {
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects");
+    table
+      .integer("task_id")
+      .notNullable()
+      .references("id")
+      .inTable("task");
+    table.primary(["project_id", "task_id"]);
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema
+exports.down = async knex => {
+  await knex.schema
     .dropTableIfExists("projects_tasks")
     .dropTableIfExists("tasks")
     .dropTableIfExists("resources")
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./dev.sqlite3"
+      filename: "./data/projects.db3"
     },
     migrations: {
       directory: "./data/migrations"
