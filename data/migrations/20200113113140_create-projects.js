98f94cf210bc3f3d824acf272a14fb85ca0a5ff8
Carlos <miklo.concepcion@gmail.com>
2020-01-13 11:38:31 -0600

    generating data flow in migrations

--- data/migrations/20200113113140_create-projects.js
+++ data/migrations/20200113113140_create-projects.js
@@ -1,8 +1,30 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("projects", table => {
+      table.increments("id");
+      table
+        .text("name", 100)
+        .unique()
+        .notNullable();
+      table.text("description").notNullable();
+      table.boolean("completed").defaultTo("false");
+    })
+
+    .createTable("actions", table => {
+      table.increments("id");
+      table
+        .integer("project_id")
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table.text("description").notNullable();
+      table.text("notes").notNullable();
+      table.boolean("completed").defaultTo("false");
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("projects").dropTableIfExists("actions");
 };
