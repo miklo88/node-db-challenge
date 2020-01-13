e5a1f162703f806fffba5f4cfa45fb0b6b5028c8
Carlos <miklo.concepcion@gmail.com>
2020-01-13 11:13:38 -0600

    gettin back into enviornment

---
+++ api/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+// SERVER
+const server = express();
+
+// SERVER API
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "API TEST"
+  });
+});
+
+// SERVER API ERROR HANDLER
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "API FAIL"
+  });
+});
+
+module.exports = server;
