9f7377973b78edd5eaa62482ef15a4baab70bf75
Carlos <miklo.concepcion@gmail.com>
2020-01-13 13:16:26 -0600

    mvp hit

--- knexfile.js
+++ knexfile.js
@@ -3,54 +3,21 @@
 module.exports = {
   development: {
     client: "sqlite3",
-    useNullAsDefault: true,
     connection: {
       filename: "./data/projects.db3"
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
     seeds: {
       directory: "./data/seeds"
-    }
-  },
-  // needed when using foreign keys
-  pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
     }
   }
 };
-// staging: {
-//   client: "postgresql",
-//   connection: {
-//     database: "my_db",
-//     user: "username",
-//     password: "password"
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: "knex_migrations"
-//   }
-// },
-
-// production: {
-//   client: "postgresql",
-//   connection: {
-//     database: "my_db",
-//     user: "username",
-//     password: "password"
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: "knex_migrations"
-//   }
-//   }
-// };
